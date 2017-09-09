3ab3345c5d4e40ca5729f4857ad444cfe41390e8
YSJ laptop <shaojieyew@gmail.com>
2017-09-09 23:55:46 +0800

    check in

---
+++ dblp_parser.py
@@ -0,0 +1,105 @@
+import xml.etree.ElementTree as ET
+import csv
+import string
+from w3lib.html import replace_entities
+from bs4 import BeautifulSoup
+
+#initalise reader and writer
+fr = open( "dblp.xml", "r" )
+#fw = open("test.txt","w+")
+fw = open('dblp.csv', 'w', encoding='utf-8')
+csvwriter = csv.writer(fw, lineterminator="\n")
+count=0
+record = ''
+started = 0
+types = ["article","inproceedings","proceedings","book","incollection",
+                "phdthesis","mastersthesis","www","data"];
+cols=["key","type","author","title","booktitle","pages","year","address","journal","volume","number","month","url","ee"]#,"publisher","crossref","isbn","series","school","chapter"]
+
+#write the table header to csv
+header =[]
+for col in cols:
+    header.append(col)
+csvwriter.writerow(header)
+
+
+#remove special character for parse
+def remove_non_ascii(text):
+    text = replace_entities(text);
+    return text;
+#check if its the start of an publiObject, if not return nothing
+def get_start_parent_element(text):
+    for publiType in types:
+        if ('<'+publiType) in words:
+            return '<'+publiType;
+#check if its the end of an publiObject, if not return nothing
+def get_end_parent_element(text):
+    for publiType in types:
+        if ('</'+publiType+'>') in words:
+            return '</'+publiType+'>';
+
+#process the information of a publiObject
+def endPubli(record, endTag):
+    started =0
+    record = record+endTag
+    #record = remove_non_ascii(record)
+    print (count)
+    try:
+        soup = BeautifulSoup(record)
+        #tree = ET.fromstring(record)
+    except:
+        print( record)
+        print ("Unexpected error:"+ sys.exc_info()[0])
+    publi =[]
+    #key = tree.attrib['key']
+    tree = soup.find("body").contents[0]
+    print (tree)
+    key = tree['key']
+    publi.append(key)
+    publiType = tree.name
+    publi.append(publiType)
+    authors = "{"
+    for member in tree.find_all('author'):
+        authors=authors+member.text+","
+    if len(authors)>1:
+        authors=authors[:len(authors)-1]
+    authors=authors+"}"
+    publi.append(authors)
+
+    for col in cols:
+        if col!='author' and col!='key' and col!='type':
+            value = tree.find(col)
+            if value is not None:
+                value=value.text
+            publi.append(value)
+
+    print (publi)
+    csvwriter.writerow(publi)
+
+#read line by line XML
+for line in fr:
+    words = line
+    startPubliType =  get_start_parent_element(words);
+    endPubliType =  get_end_parent_element(words);
+
+    #if it is end of an object, stop recoding the line and process the object
+    if endPubliType is not None and endPubliType in words:
+        count=count+1
+        endPubli(record,(endPubliType))
+        record=''
+        #if count==200:
+        #    break
+
+    #if it is start of an object, start to record the lines
+    if startPubliType is not None and (startPubliType) in words:
+        started=1
+        record=''
+        if endPubliType is not None:
+            words = str.replace(words, (endPubliType),'')
+
+    #only record line if the head of the object was found
+    if started==1:
+        record = record+words;
+
+#end of the file, close the csv file
+fw.close()
